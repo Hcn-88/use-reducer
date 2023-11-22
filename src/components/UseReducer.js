a105b4c2c5246ec23fc07af950ded57df5dd63f1
Houcine <106538937+Hcn-88@users.noreply.github.com>
2023-11-22 17:05:16 -0300

    Update UseReducer.js

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -63,7 +63,7 @@ const TodoExample = () => {
       </div>
       <ul className="w-full md:w-1/2 my-8 flex flex-col gap-5">
         {todos.map((todo) => (
-          <div key={todo.id} className="flex items-center justify-between gap-3">
+          <div key={todo.id} className="flex items-center gap-3">
             <li
               className="flex-1 bg-slate-200 py-2 px-4 rounded-full"
             >
