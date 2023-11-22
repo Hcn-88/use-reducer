9453a7016bcd37c71e876c18e42bf779387283d3
Houcine <106538937+Hcn-88@users.noreply.github.com>
2023-11-22 17:03:05 -0300

    Update UseReducer.js

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -63,12 +63,11 @@ const TodoExample = () => {
       </div>
       <ul className="w-full md:w-1/2 my-8 flex flex-col gap-5">
         {todos.map((todo) => (
-          <div className="flex items-center justify-between gap-3">
+          <div key={todo.id} className="flex items-center justify-between gap-3">
             <li
-              key={todo.id}
               className="flex-1 bg-slate-200 py-2 px-4 rounded-full"
             >
-              {todo.text}{" "}
+              {todo.text.charAt(0).toUpperCase() + todo.text.slice(1)}
             </li>
             <button
               className="bg-slate-400 py-2 px-4 rounded-full hover:drop-shadow-xl"
